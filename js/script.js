894064ec9fe1122cc9fc7139c39e5048b18b3e3f
Anna <tkachenko.an.an@gmail.com>
2022-07-02 15:46:04 +0300

    script.js changing

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
-alert('hello');
+const numberOfFilms = +prompt('сколько фильмов вы уже просмотрели?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов', '');
+const b = prompt('на сколько оцените его?','');
+const c = prompt('один из последних просмотренных фильмов', '');
+const d = prompt('на сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
